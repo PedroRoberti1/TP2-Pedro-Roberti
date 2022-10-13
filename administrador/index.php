256b622eea01a5c68481cd1db2bfd8ba07a88198
Pedro Roberti <pedroroberti99@gmail.com>
2022-10-13 02:53:05 -0300

    continuamos estilizando y agregando funcionalidad a las paginas

--- administrador/index.php
+++ administrador/index.php
@@ -1,33 +1,17 @@
-<?php
-session_start();
-
-
-if (isset($_SESSION['user_id'])) {
-    $records = $conn->prepare('SELECT id, email, password FROM users WHERE id = :id');
-    $records->bindParam(':id', $_SESSION['user_id']);
-    $records->execute();
-    $results = $records->fetch(PDO::FETCH_ASSOC);
-
-    $user = null;
-
-    if (count($results) > 0) {
-        $user = $results;
-    }
-}
-?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="utf-8">
-    <title>Welcome to you WebApp</title>
+    <title>Bienvendio a CrackWatch</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
 
+    <?php require 'partials/header.php' ?>
 
     <?php if (!empty($user)) : ?>
         <br> Welcome. <?= $user['email']; ?>
@@ -36,10 +20,10 @@ if (isset($_SESSION['user_id'])) {
             Logout
         </a>
     <?php else : ?>
-        <h1>Please Login or SignUp</h1>
+        <h1>Por favor inicia sesion o crea una cuenta para acceder a esta seccion</h1>
 
-        <a href="login.php">Login</a> or
-        <a href="signup.php">SignUp</a>
+        <a href="./seccion/iniciar.php">Iniciar</a> or
+        <a href="./seccion/registrar.php">Registrarse</a>
     <?php endif; ?>
 </body>
 
---
+++ administrador/seccion/iniciar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Iniciar</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+
+<body>
+<?php require '../partials/header.php' ?>
+
+<h1>Iniciar</h1>
+<span>o <a href="login.php">Registrar</a></span>
+
+    <form action="iniciar.php" method="POST">
+        <input name="email" type="text" placeholder="Ingresa tu mail">
+        <input name="contraseña" type="password" placeholder="Ingresa tu contraseña">
+        <input type="submit" value="Send">
+    </form>
+</body>
+
+</html>
